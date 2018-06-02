d904c6ebbf8301a3c9e3ceab3de94ca0b6cf1d50
redmarbleroad <39830779+redmarbleroad@users.noreply.github.com>
2018-06-02 12:13:20 +0100

    created file
    code so far

---
+++ main.rb
@@ -0,0 +1,30 @@
+#intro
+puts "Fergal Cochrane"
+puts ""
+puts ""
+puts "████████╗██╗  ██╗███████╗     ██████╗ ██████╗ ██╗     ██████╗" 
+puts "╚══██╔══╝██║  ██║██╔════╝    ██╔════╝██╔═══██╗██║     ██╔══██╗"
+puts "   ██║   ███████║█████╗      ██║     ██║   ██║██║     ██║  ██║"
+puts "   ██║   ██╔══██║██╔══╝      ██║     ██║   ██║██║     ██║  ██║"
+puts "   ██║   ██║  ██║███████╗    ╚██████╗╚██████╔╝███████╗██████╔╝"
+puts "   ╚═╝   ╚═╝  ╚═╝╚══════╝     ╚═════╝ ╚═════╝ ╚══════╝╚═════╝ "
+puts ""
+puts "                 Compiled with Coding Ground"
+puts ""
+puts ""
+
+#menu code
+sleep 3
+puts "  7.48 9/2/02"
+sleep 3
+puts "       It's been a full year. The rivers have frozen over, and"
+puts "       they've had to abandone the non-insulated buildings. The"
+puts "       temperature reached a record low last week. -70."
+puts ""
+puts "       The cottage was a shell of a house. Animals had broken the"
+puts "       windows, and birds had built nests on the shelves and the"
+puts "       mantlepeice. Of course, there's no sign of life left here now,"
+puts "       only a few cracked eggs and downy feathers."
+puts ""
+puts "input your name"
+name = gets
